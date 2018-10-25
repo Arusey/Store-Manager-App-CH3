77fda5bdb3513433942219b7f47385f630f01445
Arusey <kevinkibetw7@gmail.com>
2018-10-25 20:43:42 +0300

    [Feature #161460025]Add endpoint for get single product

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
-from .views import  AdminSignup, AdminLogin, Product
+from .views import  AdminSignup, AdminLogin, Product, SingleProduct
 '''creates our Blueprint'''
 mydbblue = Blueprint("api", __name__, url_prefix="/api/v2")
 
@@ -9,3 +9,4 @@ api = Api(mydbblue)
 api.add_resource(AdminSignup, '/auth/adminsignup')
 api.add_resource(AdminLogin, '/auth/adminlogin')
 api.add_resource(Product, '/products')
+api.add_resource(SingleProduct, '/products/<int:id>')
